aab1e9bf7547cf959cfd7c36e37442cba71436a2
Vinniemoks <157680687+Vinniemoks@users.noreply.github.com>
2024-06-17 14:09:01 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,28 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('tailwindcss')({
+      
+      theme: {
+        extend: {
+          colors: {
+            'primary': '#3498db', 
+            'secondary': '#f5f5f5' 
+          },
+          fontSize: {
+            'xs': '.75rem', 
+            'sm': '.875rem'
+          }
+        }
+      }
+    }),
+    require('autoprefixer')({
+     
+      overrideBrowserslist: [
+        'last 2 versions', /
+        '>1%',
+        'ie 11', 
+        'maintained node versions'
+      ]
+    }),
+  ],
+};
